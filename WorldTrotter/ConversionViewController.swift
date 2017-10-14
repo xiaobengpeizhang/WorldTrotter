56a67e2513b17e5ccad1e7a30b390cfa2d7df5c4
杨佩璋 <yangpeizhang@yangpeizhangdeMacBook-Pro.local>
2017-10-14 17:36:47 +0800

    first commit

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by 杨佩璋 on 2017/10/13.
+//  Copyright © 2017年 杨佩璋. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    var numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text,  let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()    
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let existDecimal = textField.text?.range(of: ".")
+        let replaceDecimal = string.range(of: ".")
+        if existDecimal != nil, replaceDecimal != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+}
